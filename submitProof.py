81f690f90ebf2bda737c658396b049437e91d8b9
nengida1 <nengida@seas.upenn.edu>
2026-03-31 00:15:23 -0400

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -34,7 +34,7 @@ def merkle_assignment():
     addr, sig = sign_challenge(challenge)
 
     if sign_challenge_verify(challenge, addr, sig):
-        tx_hash = '0x'
+        #tx_hash = '0x'
         # TODO, when you are ready to attempt to claim a prime (and pay gas fees),
         #  complete this method and run your code with the following line un-commented
         tx_hash = send_signed_msg(proof, leaves[random_leaf_index])
